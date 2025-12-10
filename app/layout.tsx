43e4db3464f7735e89c29acef3472bcdcc3315fe
l200239188-hub <l200239188@student.ums.ac.id>
2025-12-10 17:25:31 +0700

    add global.d.ts for css module declaration

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,8 @@
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
+import type { ReactNode } from "react";
 
-export default function RootLayout({ children }) {
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <body className={`${inter.className} antialiased`}>
